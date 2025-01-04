19e38bc95024238165267da6ef8890a569c0997c
cbcb00 <babycyril@gmail.com>
2025-01-04 23:05:48 +0530

    DerpFest Adaptations

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,13 +10,16 @@ include device/oneplus/sm8350-common/BoardConfigCommon.mk
 DEVICE_PATH := device/oneplus/martini
 
 # Display
-TARGET_SCREEN_DENSITY := 450
+TARGET_SCREEN_DENSITY := 480
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
 TARGET_KERNEL_ADDITIONAL_FLAGS := CONFIG_OPLUS_CHG_OP9RT_PMIC_VOOCPHY=y
+
+# Partition
+BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 11186208768
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
